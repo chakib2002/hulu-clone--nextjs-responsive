2a32ecabda69bf70d6772520517bd8e4a1632226
chakib2002 <Abdelmalek.feddal@worldlearningalgeria.org>
2021-11-25 22:39:26 +0100

    Build-Header

---
+++ components/HeaderItem.js
@@ -0,0 +1,9 @@
+export default function HeaderItem({title, Icon}) {
+    return (
+        <div className="group flex flex-col items-center cursor-pointer 
+        w-12 sm:w-20 hover:text-white active:scale-95 active:transition active:transform active:ease-out " >
+            <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+            <p className="opacity-0 group-hover:opacity-100 tracking-widest " >{title}</p>
+        </div>
+    )
+}

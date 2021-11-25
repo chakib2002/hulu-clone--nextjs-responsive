459a88696962f895bc04c30753e74de119f17faa
chakib2002 <Abdelmalek.feddal@worldlearningalgeria.org>
2021-11-25 23:53:06 +0100

    nav-finish

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import requests from '../utils/requests'
 
 export default function Nav() {
     return (
-        <nav className="">
+        <nav className="relative">
             <div className="flex px-10 py-5 sm:px-20 text-xl whitespace-nowrap space-x-10 sm:space-x-5 overflow-x-scroll scrollbar-hide">
                 {Object.entries(requests).map(([key, {title, url}])=>(
                     <h2 key={key} className='opacity-75 cursor-pointer transition duration-300 border border-opacity-50 rounded-full w-auto py-2 px-4 hover:text-white text-xl
@@ -11,6 +11,7 @@ export default function Nav() {
                     transform active:text-xl '>{title}</h2>
                 ))}
             </div>
+            <div className="absolute top-4 right-0 bg-gradient-to-l from-[#06202A] h-14 w-1/12"/>
         </nav>
     )
 }
